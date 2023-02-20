7b217a06eefb6d802d67569176e4e27298342adb
pavelkotliarov <shpun.0613@gmail.com>
2023-02-20 11:32:40 +0300

    Add feature modal

--- src/components/Header/HeaderComponent.vue
+++ src/components/Header/HeaderComponent.vue
@@ -3,13 +3,24 @@ import LogoComponent from "./LogoComponent.vue";
 import type { MenuItemType } from "../UI/UiDropdown.vue";
 import { Bars3Icon } from "@heroicons/vue/20/solid";
 import UiDropdown from "@/components/UI/UiDropdown.vue";
+import { ref } from "vue";
+import FaqModal from "@/components/FaqModal.vue";
+import FeedbackModal from "@/components/FeedbackModal.vue";
+
+const openModal = ref<boolean>(false);
+const selectedId = ref<string | null>(null);
 const selectItem = (id: string): void => {
-  console.log("select", id);
+  selectedId.value = id;
+  openModal.value = true;
+};
+const closeModal = () => {
+  openModal.value = false;
+  selectedId.value = null;
 };
 
 const menuItems: Array<MenuItemType> = [
-  { title: "FAQ", id: "FAQ".toLowerCase() },
-  { title: "Feedback", id: "Feedback".toLowerCase() },
+  { title: "FAQ", id: "faq" },
+  { title: "Feedback", id: "feedback" },
 ];
 </script>
 <template>
@@ -24,5 +35,15 @@ const menuItems: Array<MenuItemType> = [
         :icon="Bars3Icon"
       />
     </div>
+    <faq-modal
+      v-if="selectedId === 'faq'"
+      @close="closeModal"
+      :open="openModal"
+    />
+    <feedback-modal
+      v-if="selectedId === 'feedback'"
+      @close="closeModal"
+      :open="openModal"
+    />
   </div>
 </template>
